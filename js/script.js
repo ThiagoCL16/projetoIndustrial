9d40bbbf854e4abdfd22777e179192a66a5cb1a7
ThiagoCL16 <thiagocavalcantelopes16@gmail.com>
2024-09-08 14:06:02 -0300

    Modelo de carta

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//Math.random() - gera um numero aleatorio
+/*
+let img = new Array(36)
+img[0] = '<img src="img/gato.png">'
+*/
+
+let carta = document.getElementById('carta')
+
+carta.onclick = function() {
+    carta.style.transform = 'rotateY(180deg)'
+}
